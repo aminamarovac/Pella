65f490af4ee68a9ad85d6a551f47c8134a4a670d
Amina <107712393+aminamarovac@users.noreply.github.com>
2023-08-15 21:48:18 +0200

    pella

---
+++ ok.js
@@ -0,0 +1,6 @@
+const toggleSidebar = document.getElementById('toggleSidebar');
+const sidebar = document.getElementById('sidebar');
+
+toggleSidebar.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
